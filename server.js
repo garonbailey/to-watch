cb11d0cf9e2aa0022382066896cca974fae140ce
garonbailey <bailegm@gmail.com>
2015-12-21 21:20:23 -0600

    server changes, updating for future functionality

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express        = require('express'),
 	server         = express(),
 	bodyParser     = require('body-parser'),
-	mongoose       = require('mongoose'),
 	methodOverride = require('method-override'),
+	mongoose       = require('mongoose'),
 	Schema         = mongoose.Schema;
+
+// Mongoose
 
 var userSchema = new Schema({
 	username: ,
@@ -13,11 +15,28 @@ var userSchema = new Schema({
 
 var User = mongoose.model('User', userSchema);
 
+//Server Usage and Settings
+
 server.use(express.static('./public'));
-server.use(bodyParser({ extended: true }));
+server.use(bodyParser.urlEncoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(methodOverride('_method'));
 
 // server gets and posts
+
+server.get('/user', function (req, res) {
+
+});
+
+server.post('/user', function (req, res) {
+
+});
+
+server.post('/user/:id', function (req, res) {
+
+});
+
+//DB and Server Listen
 
 mongoose.connect('mongodb://localhost:27017/towatch');
 server.listen(3000, function () {
