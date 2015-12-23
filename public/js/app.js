9893aa92d6f71d0bc6930dfcf174f813cf9ec377
garonbailey <bailegm@gmail.com>
2015-12-23 04:09:38 -0600

    updated angular routing to work, mild styling updates to login page, need to get api functional

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,14 @@ var app = angular.module('ToWatch', ['ngRoute']);
 app.controller('getUserCtrl', ['$routeParams', '$http', function ($routeParams, $http) {
 	var ctrl = this;
 
-	this.getUser = function () {
-		$http.get('/user')
-			.then(function success (res) {
+	// this.getUser = function () {
+	// 	$http.get('/user')
+	// 		.then(function success (res) {
 
-			}, function error (res) {
+	// 		}, function error (res) {
 
-			});
-	};
+	// 		});
+	// };
 }]);
 
 app.controller('loginCtrl', ['$routeParams', '$http', function ($routeParams, $http) {
@@ -38,35 +38,35 @@ app.controller('userCtrl', ['$routeParams', '$http', function ($routeParams, $ht
 }]);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-	$locationProvider.html5Mod({ enabled: true });
+	$locationProvider.html5Mode({ enabled: true });
 	$routeProvider.
 		when('/', {
-			templateUrl: '/views/index.html',
+			templateUrl: 'views/index.html',
 			controller: 'getUserCtrl',
 			controllerAs: 'ctrl'
 		}).
 		when('/signup', {
-			templateUrl: '/views/signup.html',
+			templateUrl: 'views/signup.html',
 			controller: 'signupCtrl',
 			controllerAs: 'ctrl'
 		}).
 		when('/login', {
-			templateUrl: '/views/login.html',
+			templateUrl: 'views/login.html',
 			controller: 'loginCtrl',
 			controllerAs: 'ctrl'
 		}).
 		when('/search', {
-			templateUrl: '/views/search.html',
+			templateUrl: 'views/search.html',
 			controller: 'searchCtrl',
 			controllerAs: 'ctrl'
 		}).
 		when('/profile', {
-			templateUrl: '/views/profile.html',
+			templateUrl: 'views/profile.html',
 			controller: 'userCtrl',
 			controllerAs: 'ctrl'
 		}).
 		when('/404', {
-			templateUrl: '/views/404.html',
+			templateUrl: 'views/404.html',
 			controller: 'errorCtrl',
 			controllerAs: 'ctrl'
 		}).
--- server.js
+++ server.js
@@ -23,23 +23,27 @@ var User = mongoose.model('User', userSchema);
 //Server Usage and Settings
 
 server.use(express.static('./public'));
-server.use(bodyParser.urlEncoded({ extended: true }));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(methodOverride('_method'));
 
 // server gets and posts
 
-server.get('/user', function (req, res) {
+server.get('/users', function (req, res) {
 	User.findOne()
 });
 
-server.post('/user', function (req, res) {
+server.post('/users', function (req, res) {
 
 });
 
-server.post('/user/:id', function (req, res) {
 
+// App render
+
+server.get('*', function(req, res) {
+    res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
+
 
 //DB and Server Listen
 
